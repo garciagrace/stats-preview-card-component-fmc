cc66f9d6dd487c0007bb71a3066ac7330c4d407b
Grace Garcia <gracegarcia0803@gmail.com>
2022-04-24 02:16:42 +0800

    Added style using tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,11 +16,17 @@ module.exports = {
         offWhite: 'hsla(0, 0%, 100%, 0.75)',
         offWhiteSlight: 'hsla(0, 0%, 100%, 0.6)',
       },
+      backgroundImage: {
+        mobile:
+          'linear-gradient(to right, hsla(277, 64%, 50%, 0.6), hsla(277, 64%, 50%, 0.6)), url("../images/image-header-mobile.jpg")',
+        desktop:
+          'linear-gradient(to right, hsla(277, 64%, 50%, 0.6), hsla(277, 64%, 50%, 0.6)), url("../images/image-header-desktop.jpg")',
+      },
     },
     screens: {
-      sm: '375px',
-      md: '800px',
-      lg: '1080px',
+      sm: '320px',
+      md: '600px',
+      lg: '976px',
     },
   },
   plugins: [],
